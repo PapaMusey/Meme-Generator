bb9ba1c8eb91ecbf37002c36b541cbf6674def7c
Andy Musey <p.emusey@gmail.com>
2024-08-15 16:01:14 +0000

    update

--- src/indexes.js
+++ src/indexes.js
@@ -1,16 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom";
+// import React from "react";
+// import ReactDOM from "react-dom";
 
-function App () {
-    const thingsArray = ["Thing 1", "Thing 2"]
-    const newthingsArray = thingsArray.map(thing => <p key={thing} >Show {thing} </p>)  
+// function App () {
+//     const thingsArray = ["Thing 1", "Thing 2"]
+//     const newthingsArray = thingsArray.map(thing => <p key={thing} >Show {thing} </p>)  
 
+//     function addItem() {
+//         const newThingText =`Thing ${thingsArray.length + 1}`
+//         thingsArray.push(newThingText)
+//         console.log(thingsArray)
+//     }
 
-    return(
-        <div>
-            <button>Add Item</button>
+//     return(
+//         <div>
+//             <button onClick={addItem}>Add Item</button>
             
-          
-        </div>
-    )
-}
+//           </div>
+//     )
+// }
